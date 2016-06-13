4a2f0dea75e85397aa5224e48fe941790d63807e
Audren <audren.burlot@gmail.com>
2016-06-13 13:49:40 +0200

    'refacto'

--- server.js
+++ server.js
@@ -1,23 +1,27 @@
-var express = require("express");
-var path = require("path");
+// server.js
+
+var express    = require("express");
+var path 	     = require("path");
 var bodyParser = require("body-parser");
 
 var app = express();
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json());
 
-// Initialize the app.
-var server = app.listen(process.env.PORT || 8080, function () {
-var port = server.address().port;
-	console.log("App now running on port", port);
+var router = express.Router();
+router.use(function(req, res, next) {
+	console.log('smth is happening');
+	next();
 });
 
-// Generic error handler used by all endpoints.
-function handleError(res, reason, message, code) {
-  console.log("ERROR: " + reason);
-  res.status(code || 500).json({"error": message});
-}
+router.get('/', function(req, res) {
+	res.json({ message: 'Test route working !'});
+});
 
-app.get("/test", function(req, res) {
-	res.status(200);
+app.use('/api', router);
+
+// Initialize the app.
+var server = app.listen(process.env.PORT || 8080, function () {
+	var port = server.address().port;
+	console.log("App now running on port", port);
 });
