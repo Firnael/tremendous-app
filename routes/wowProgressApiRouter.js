af57791d386a079da6257e96e43c213bee5fc3ab
Nael <audren.burlot@gmail.com>
2016-06-16 00:57:35 +0200

    stuff

--- routes/wowProgressApiRouter.js
+++ routes/wowProgressApiRouter.js
@@ -1,7 +1,18 @@
-/**
- * Created with JetBrains WebStorm.
- * User: Audren
- * Date: 15/06/16
- * Time: 19:26
- * To change this template use File | Settings | File Templates.
- */
+var request = require('request');
+var express = require('express');
+var wowProgressRouter = express.Router();
+var url = "http://www.wowprogress.com/guild/eu/ysondre/Millenium/json_rank";
+
+// Test route
+wowProgressRouter.get('/', function(req, res) {
+    request(url, function (err, response, body) {
+        if (!err && response.statusCode == 200) {
+            res.send(body);
+        } else {
+            console.log(err);
+        }
+    })
+});
+
+
+module.exports = wowProgressRouter;
