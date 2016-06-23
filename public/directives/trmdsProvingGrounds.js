cd2ba1190dfa1bfe682bc824143aaf51fbb8bdf1
Audren <audren.burlot@gmail.com>
2016-06-23 18:04:40 +0200

    character dashboard

---
+++ public/directives/trmdsProvingGrounds.js
@@ -0,0 +1,43 @@
+/* Directive trmdsProvingGrounds.js */
+(function() {
+    'use strict';
+
+    angular
+        .module('app')
+        .directive('trmdsProvingGrounds', trmdsProvingGroundsDirective);
+
+    function trmdsProvingGroundsDirective() {
+
+        function link (scope, element, attributes) {
+            scope.$watch('value', function(newValue, oldValue) {
+                if (newValue){
+                    var tmp;
+                    switch(newValue) {
+                        case '0': tmp = ''; break;
+                        case '1': tmp = 'bronze'; break;
+                        case '2': tmp = 'silver'; break;
+                        case '3': tmp = 'gold'; break;
+                        default: tmp = ''; break;
+                    }
+
+                    var url = 'https://wow.zamimg.com/images/wow/icons/large/achievement_challengemode_';
+                    if(tmp === '') {
+                      url = 'https://hydra-media.cursecdn.com/wow.gamepedia.com/9/97/Inv_misc_questionmark.png';
+                    } else {
+                      url += tmp + '.jpg';
+                    }
+                    scope.result = url;
+                }
+            }, false);
+        };
+
+        return {
+            restrict: "E",
+            templateUrl: 'views/directives/proving-grounds.html',
+            link: link,
+            scope: {
+              value: '@'
+            }
+        };
+    }
+})();
