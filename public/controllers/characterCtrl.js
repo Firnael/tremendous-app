8549a329cfe62c71f5cbe73352b542517092febd
Audren <audren.burlot@gmail.com>
2016-08-18 18:29:39 +0200

    reroll dans la fiche character en vue de refonte page roster

--- public/controllers/characterCtrl.js
+++ public/controllers/characterCtrl.js
@@ -11,16 +11,19 @@
         var vm = this;
         vm.updatingCharacter = true;
         vm.character = {};
+        vm.rerolls = {};
 
         // Chart Professions
         vm.labels = ['Accompli', 'Reste'];
         vm.colors = ['#71c326','#555555'];
 
         vm.getCharacter = getCharacter;
+        vm.getRerolls = getRerolls;
         vm.getThumbnailPath = getThumbnailPath;
         vm.getProfessionData = getProfessionData;
         vm.getSelectedSpec = getSelectedSpec;
         vm.updateCharacter = updateCharacter;
+        vm.getClassColor = getClassColor;
         activate();
 
         //////////////
@@ -31,20 +34,29 @@
         }
 
         function getCharacter() {
-            CharacterSvc.getCharacter($routeParams.characterName).then(function (result) {
-                vm.character = result;
-                if(typeof vm.character.class == 'undefined') {
-                    return updateCharacter();
-                }
+          CharacterSvc.getCharacter($routeParams.characterName).then(function (result) {
+            vm.character = result;
+            if(typeof vm.character.class == 'undefined') {
+                return updateCharacter();
+            }
 
-                vm.professionData = [];
-                for(var i=0; i<6; i++) {
-                  vm.professionData[i] = vm.getProfessionData(i);
-                }
+            vm.professionData = [];
+            for(var i=0; i<6; i++) {
+              vm.professionData[i] = vm.getProfessionData(i);
+            }
 
-                vm.lastModified = moment(vm.character.lastModified).calendar();
-                vm.updatingCharacter = false;
-            });
+            vm.lastModified = moment(vm.character.lastModified).calendar();
+            vm.updatingCharacter = false;
+
+            // Get rerolls
+            vm.getRerolls(vm.character.accountIdentifier);
+          });
+        }
+
+        function getRerolls(accountId) {
+          CharacterSvc.getByAccountId(accountId).then(function (result) {
+            vm.rerolls = result;
+          });
         }
 
         function getThumbnailPath() {
@@ -60,7 +72,7 @@
           if(!vm.character.professions[index]) {
             return [];
           }
-          
+
           var data = [];
           var rank = vm.character.professions[index].rank;
           var max = vm.character.professions[index].max;
@@ -91,6 +103,10 @@
             CharacterSvc.updateCharacter($routeParams.characterName).then(function (result) {
                 getCharacter();
             });
+        }
+
+        function getClassColor(value) {
+          return UtilsSvc.getCssClassByCharacterClass(value);
         }
     }
 })();
