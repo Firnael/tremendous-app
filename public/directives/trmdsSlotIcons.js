8cac1f305ede375b0e21cbcb4e10243cbb000804
Audren <audren.burlot@gmail.com>
2016-08-23 13:46:51 +0200

    icons slots roster

--- public/directives/trmdsSlotIcons.js
+++ public/directives/trmdsSlotIcons.js
@@ -10,32 +10,29 @@
 
       function link (scope, element, attributes) {
         switch(scope.slot) {
-          case 'head': scope.src = 'http://vignette2.wikia.nocookie.net/wowwiki/images/c/c3/Ui-paperdoll-slot-head.png'; break;
-          case 'neck': scope.src = 'http://vignette1.wikia.nocookie.net/wowwiki/images/d/d4/Ui-paperdoll-slot-neck.png'; break;
-          case 'shoulder':
-          scope.src = 'http://vignette1.wikia.nocookie.net/wowwiki/images/f/fa/Ui-paperdoll-slot-shoulder.png'; break;
-          case 'back': scope.src = 'http://vignette3.wikia.nocookie.net/wowwiki/images/f/f4/Ui-paperdoll-slot-back.png'; break;
-          case 'chest': scope.src = 'http://vignette4.wikia.nocookie.net/wowwiki/images/b/b7/Ui-paperdoll-slot-chest.png'; break;
-          case 'wrist': scope.src = 'http://vignette3.wikia.nocookie.net/wowwiki/images/1/1d/Ui-paperdoll-slot-wrists.png'; break;
-          case 'hands': scope.src = 'http://vignette3.wikia.nocookie.net/wowwiki/images/2/22/Ui-paperdoll-slot-hands.png'; break;
-          case 'waist': scope.src = 'http://vignette1.wikia.nocookie.net/wowwiki/images/c/cd/Ui-paperdoll-slot-waist.png'; break;
-          case 'legs': scope.src = 'http://vignette3.wikia.nocookie.net/wowwiki/images/1/14/Ui-paperdoll-slot-legs.png'; break;
-          case 'feet': scope.src = 'http://vignette3.wikia.nocookie.net/wowwiki/images/a/a5/Ui-paperdoll-slot-feet.png'; break;
-          case 'finger1': scope.src = 'http://vignette1.wikia.nocookie.net/wowwiki/images/c/c2/Ui-paperdoll-slot-finger.png'; break;
-          case 'finger2': scope.src = 'http://vignette1.wikia.nocookie.net/wowwiki/images/c/c2/Ui-paperdoll-slot-finger.png'; break;
-          case 'trinket1':
-          scope.src = 'http://vignette3.wikia.nocookie.net/wowwiki/images/2/26/Ui-paperdoll-slot-trinket.png'; break;
-          case 'trinket2':
-          scope.src = 'http://vignette3.wikia.nocookie.net/wowwiki/images/2/26/Ui-paperdoll-slot-trinket.png'; break;
-          case 'mainHand': scope.src = 'http://vignette3.wikia.nocookie.net/wowwiki/images/f/f5/Ui-paperdoll-slot-mainhand.png'; break;
-          case 'offHand': scope.src = 'http://vignette2.wikia.nocookie.net/wowwiki/images/3/30/Ui-paperdoll-slot-secondaryhand.png'; break;
+          case 'head': scope.src = 'http://wow.zamimg.com/images/wow/icons/large/inv_helmet_03.jpg'; break;
+          case 'neck': scope.src = 'http://wow.zamimg.com/images/wow/icons/large/inv_jewelry_necklace_30naxxramas.jpg'; break;
+          case 'shoulder': scope.src = 'http://wow.zamimg.com/images/wow/icons/large/inv_shoulder_30.jpg'; break;
+          case 'back': scope.src = 'http://wow.zamimg.com/images/wow/icons/large/inv_misc_cape_11.jpg'; break;
+          case 'chest': scope.src = 'http://wow.zamimg.com/images/wow/icons/large/inv_chest_chain.jpg'; break;
+          case 'wrist': scope.src = 'http://wow.zamimg.com/images/wow/icons/large/inv_bracer_03.jpg'; break;
+          case 'hands': scope.src = 'http://wow.zamimg.com/images/wow/icons/large/inv_gauntlets_05.jpg'; break;
+          case 'waist': scope.src = 'http://wow.zamimg.com/images/wow/icons/large/inv_belt_24.jpg'; break;
+          case 'legs': scope.src = 'http://wow.zamimg.com/images/wow/icons/large/inv_pants_02.jpg'; break;
+          case 'feet': scope.src = 'http://wow.zamimg.com/images/wow/icons/large/inv_boots_01.jpg'; break;
+          case 'finger1': scope.src = 'http://wow.zamimg.com/images/wow/icons/large/inv_jewelry_ring_02.jpg'; break;
+          case 'finger2': scope.src = 'http://wow.zamimg.com/images/wow/icons/large/inv_jewelry_ring_22.jpg'; break;
+          case 'trinket1': scope.src = 'http://wow.zamimg.com/images/wow/icons/large/inv_jewelry_talisman_13.jpg'; break;
+          case 'trinket2': scope.src = 'http://wow.zamimg.com/images/wow/icons/large/inv_jewelry_trinketpvp_02.jpg'; break;
+          case 'mainHand': scope.src = 'http://wow.zamimg.com/images/wow/icons/large/inv_sword_23.jpg'; break;
+          case 'offHand': scope.src = 'http://wow.zamimg.com/images/wow/icons/large/inv_shield_04.jpg'; break;
           default: break;
         }
       };
 
       return {
           restrict: "E",
-          template: '<img class="img" ng-src="{{ src }}" title="{{ slot }}" width="{{ size }}" height="{{ size }}"/>',
+          template: '<img class="img-rounded" ng-src="{{ src }}" title="{{ slot }}" width="{{ size }}" height="{{ size }}"/>',
           link: link,
           scope: {
             slot: '@',
