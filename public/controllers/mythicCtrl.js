e1bcac3a816582695f79cc7de4c85eb4b33422be
Audren <audren.burlot@gmail.com>
2016-09-06 14:32:17 +0200

    mythic plus css

--- public/controllers/mythicCtrl.js
+++ public/controllers/mythicCtrl.js
@@ -5,16 +5,19 @@
         .module('app')
         .controller('mythicCtrl', MythicCtrl);
 
-    MythicCtrl.$inject = ['CharacterSvc'];
+    MythicCtrl.$inject = ['CharacterSvc', 'UtilsSvc'];
 
-    function MythicCtrl(CharacterSvc){
+    function MythicCtrl(CharacterSvc, UtilsSvc){
         var vm = this;
         vm.roster = [];
+        vm.selectedDungeon = undefined;
         vm.updating = true;
         vm.lastUpdate = new Date().getTime();
 
         vm.getRoster = getRoster;
         vm.update = update;
+        vm.selectDungeon = selectDungeon;
+        vm.getClassColor = getClassColor;
         activate();
 
         //////////////
@@ -33,6 +36,15 @@
 
         function update() {
           console.log('Update Mythic+');
+        }
+
+        function selectDungeon(dungeonId) {
+          vm.selectedDungeon = dungeonId;
+          console.log('vm.selectedDungeon: ' + vm.selectedDungeon);
+        }
+
+        function getClassColor(value) {
+          return UtilsSvc.getCssClassByCharacterClass(value, false);
         }
     }
 })();
