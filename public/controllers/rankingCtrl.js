2dd22cb0737b1c09dba29c167c704e2a10518443
Audren <audren.burlot@gmail.com>
2016-11-29 13:34:23 +0100

    color ranking

--- public/controllers/rankingCtrl.js
+++ public/controllers/rankingCtrl.js
@@ -18,6 +18,7 @@
       vm.getGuildRanking = getGuildRanking;
       vm.updateRanking = updateRanking;
       vm.getTierProgress = getTierProgress;
+      vm.getProgressValue = getProgressValue;
 
       activate();
 
@@ -62,7 +63,34 @@
 
       function getTierProgress(progress) {
         var array = progress.split(' ');
-        return array[0] + ' ' + array[1];
+        return array[0] + ' ' + array[1] + ' ' + array[2] + ' ' + array[3];
+      }
+
+      function getProgressValue(progress) {
+        var array = progress.split(' ');
+        var emeraldNightmare = parseInt(array[0].substring(0,1));
+        if(array[1].indexOf('M') >= 0) {
+          emeraldNightmare += 7;
+        }
+        var trialOfValor = parseInt(array[2].substring(0,1));
+        if(array[3].indexOf('M') >= 0) {
+          trialOfValor += 3;
+        }
+
+        var final = emeraldNightmare + trialOfValor;
+        if(final === 20) {
+          return 'legendary';
+        } else if(final === 19) {
+          return 'epic';
+        } else if(final === 18) {
+          return 'rare';
+        } else if(final === 17) {
+          return 'uncommon';
+        } else if(final === 16) {
+          return 'common';
+        } else {
+          return 'poor';
+        }
       }
     }
 })();
